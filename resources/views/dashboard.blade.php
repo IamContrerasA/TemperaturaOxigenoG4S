74165732aa078f2ed695b6f360165d610ae77a1b
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-09-22 17:52:55 -0500

    edit views

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,49 @@
+@extends("layouts.template")
+
+@section("contenido")    
+  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">{{$paciente}}</h1>
+      <p id="demo">a</p>
+      <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group mr-2">
+          <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+        </div>
+        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+          <span data-feather="calendar"></span>
+          This week
+        </button>
+      </div>
+    </div>
+
+    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+    <h2>Resultados</h2>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Temperatura</th>
+            <th>Saturación de Oxigeno</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($results as $result)
+          <tr>
+            <td>{{$result->created_at}}</td>
+            <td>{{$result->temperature}}</td>
+            <td>{{$result->oxygen_saturation}}</td>            
+          </tr>
+        @endforeach 
+        </tbody>
+      </table>
+    </div>     
+  </main>  
+  <script>
+    var appSettings = "{{$results}}";
+ </script>
+@endsection
+
--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -21,11 +21,42 @@
       </div>
       -->
     </div>
-    <h2>Mostrar lista de usuarios con sus datos y a la derecha un boton para ver sus estadisticas, en si es como el index pero ya con los datos obtenidos</h2>
-    
+
+    @if(count($pacientes))
+
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Nombre</th>
+          <th scope="col">Edad</th>
+          <th scope="col">Sexo</th>
+          <th scope="col">DNI</th>
+          <th scope="col">Resultados</th>          
+        </tr>
+      </thead>
+      <tbody>
+
+      @foreach($pacientes as $paciente)
+        <tr>
+          <td>{{$paciente->name}}</td>
+          <td>{{$paciente->age}}</td>
+          <td>{{$paciente->sex}}</td>
+          <td>{{$paciente->DNI}}</td>
+          <td><a href= "/paciente/{{ $paciente -> id }}/resultados"> resultado </a></td>
+        </tr>
+      @endforeach 
+
+      </tbody>
+    </table>
+      
+    @else
+      {{"No existen pacientes registrados"}}
+    @endif   
+   
+
     <p>This example writes "Hello JavaScript!" into an HTML element with id="demo":</p>
 
-    <p id="demo"></p>
+    <p id="demo">a</p>
 
     <script>
     document.getElementById("demo").innerHTML = "Hello JavaScript!";
