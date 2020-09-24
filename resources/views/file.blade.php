58afa38917f38e0ef9ea2e3665773159c7625e4a
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-09-24 15:47:05 -0500

    add another form to upload files

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -24,9 +24,55 @@
       </div>
     </div>
 
+    
+    <form action="/subir_archivo" method="get">
+      @csrf
+      <div class="input-group" style="margin:100px auto">      
+          <div class="input-group-prepend">          
+            <input class="input-group-text" id="inputGroupFileAddon01" type="submit" value="Subir" name="submit">
+          </div>
+          <div class="custom-file">
+            <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+            <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label>
+          </div>    
+      </div>
+   </form>
+   
+   <div id="div1"><p>Let jQuery AJAX Change This Text</p></div>
+   <button>ver datos</button>
+
+    <?php
+      echo "</br>";echo "</br>";echo "</br>";echo "</br>";echo "</br>";      
+      echo Form::open(array('url' => '/uploadfile','files'=>'true'));
+      echo 'Select the file to upload.';
+      echo Form::file('image');
+      echo Form::submit('Upload File');
+      echo Form::close();
+    ?>
   </main>
 
-  
+  <script>    
+    // Add the following code if you want the name of the file appear on select
+    //poner nombre en la barra de busqueda del archivo
+    $(".custom-file-input").on("change", function() {
+      var fileName = $(this).val().split("\\").pop();
+      $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+  </script>
+
+  <script>
+    //actualizar la pagina y mostrar la info de uin botton
+    $(document).ready(function(){
+      $("button").click(function(){
+        $.ajax({        
+          url: "/subir_archivo", 
+          success: function(result){
+            $("#div1").html(result);
+        }});
+      });
+    });
+  </script>
+
 @endsection
 
 
