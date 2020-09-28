1ff11ef1b8e9163420e02691a5c701c12156a005
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-09-28 15:03:36 -0500

    update migrations for worker intead of patient

--- database/migrations/2020_09_22_182230_create_results_table.php
+++ database/migrations/2020_09_22_182230_create_results_table.php
@@ -15,7 +15,7 @@ class CreateResultsTable extends Migration
     {
         Schema::create('results', function (Blueprint $table) {
             $table->id();            
-            $table->integer("patient_id");
+            $table->integer("worker_id");
             $table->decimal("oxygen_saturation", 8, 2);
             $table->decimal("temperature", 8, 2);
             $table->timestamps();
