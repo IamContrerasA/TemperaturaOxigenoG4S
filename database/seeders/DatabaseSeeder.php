fc44b0982e7dd117193f6a9d4731eaefed82c614
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-09-28 15:03:50 -0500

    add seeders for roles and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,12 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use \App\Models\Patient;
+
+//importar modelos necesarios
+use \App\Models\Worker;
 use \App\Models\Result;
+use \App\Models\Role;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,12 +21,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();  
-        $num_patients = 6;
+        //POBLAR LA BASE DE DATOS
+
+        // Crear trabajadores y asignarles un random de resultados
+        $num_workers = 6;
         $mul_results = 6;
         
-        for ($i=0; $i < $num_patients; $i++) {
-            Patient::create([
+        for ($i=0; $i < $num_workers; $i++) {
+            Worker::create([
                     'name' => "Persona" . $i,
                     'age' => $i + 31,
                     'sex' => "hombre/mujer",
@@ -31,13 +37,25 @@ class DatabaseSeeder extends Seeder
         }
         
         
-        for ($i=0; $i < $num_patients * $mul_results; $i++) {
+        for ($i=0; $i < $num_workers * $mul_results; $i++) {
             Result::create([
-                    'patient_id' => mt_rand(1,$num_patients),
-                    'temperature' => 36.5 + mt_rand(1,$num_patients),
-                    'oxygen_saturation' => 89.5 +mt_rand(1,$num_patients),
+                    'worker_id' => mt_rand(1,$num_workers),
+                    'temperature' => 36.5 + mt_rand(1,$num_workers),
+                    'oxygen_saturation' => 89.5 +mt_rand(1,$num_workers),
                     'created_at' => now()
                 ]);
         }
+
+        //crear roles                
+        Role::create(['name' => "administrador"]);
+        Role::create(['name' => "operador"]);
+        Role::create(['name' => "supervisor"]);
+        Role::create(['name' => "otro"]);
+        
+        //crear usuarios
+        User::create(['role_id' => 1, 'name' => "Administrador",  'email' => "administrador@gamt.com.pe", 'password' => Hash::make("administrador")]);
+        User::create(['role_id' => 2, 'name' => "Operador",  'email' => "operador@gamt.com.pe", 'password' => Hash::make("operador")]);
+        User::create(['role_id' => 3, 'name' => "Supervisor",  'email' => "supervisor@gamt.com.pe", 'password' => Hash::make("supervisor")]);
+
     }
 }
