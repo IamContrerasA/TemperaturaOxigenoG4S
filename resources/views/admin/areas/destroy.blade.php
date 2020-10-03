be75bc1e71ad660655c623f5b187679de42b4775
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-10-02 23:13:03 -0500

    create areas crud

---
+++ resources/views/admin/areas/destroy.blade.php
@@ -0,0 +1,20 @@
+<!--Pantalla principal de un area-->
+@extends("layouts.template")
+
+@section("contenido") 
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">INDEX ADMIN AREAS</h1>
+  <!--<p id="demo">a</p>-->
+  <div class="btn-toolbar mb-2 mb-md-0">
+    <div class="btn-group mr-2">
+    <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+    <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+  </div>
+    <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+      <span data-feather="calendar"></span>
+      This week
+    </button>
+  </div>
+</div>
+@endsection
