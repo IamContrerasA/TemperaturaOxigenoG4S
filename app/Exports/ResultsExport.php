b80f568174d3cc0a4ee4f94560dbe34a5031165e
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-10-05 12:11:25 -0500

    add headings in reports

--- app/Exports/ResultsExport.php
+++ app/Exports/ResultsExport.php
@@ -4,10 +4,11 @@ namespace App\Exports;
 
 use App\Models\Result;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 //Exporta un archivo excel, segun el modelo escogido
 
-class ResultsExport implements FromCollection
+class ResultsExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -15,5 +16,10 @@ class ResultsExport implements FromCollection
     public function collection()
     {
         return Result::all();
+    }
+
+    public function headings(): array
+    {
+        return ["id", "worker_id", "oxygen_saturation", "temperature", "date", "created_at", "updated_at", ];
     }
 }
--- app/Exports/WorkersExport.php
+++ app/Exports/WorkersExport.php
@@ -4,10 +4,11 @@ namespace App\Exports;
 
 use App\Models\Worker;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 //Exporta un archivo excel, segun el modelo escogido
 
-class WorkersExport implements FromCollection
+class WorkersExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -15,6 +16,11 @@ class WorkersExport implements FromCollection
     public function collection()
     {
         return Worker::all();
+    }
+
+    public function headings(): array
+    {
+        return ["id", "name", "age", "sex", "DNI", "area_id", "roster_id", "fecha_subida", "fecha_bajada", "created_at", "updated_at",];
     }
     
 }
