6ba9ddbaaea7433c40fc0e09a58726b3258d91f3
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-10-06 17:16:21 -0500

    change index.php to url of heroku

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Origin: https://pacific-caverns-02399.herokuapp.com/');  
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 /*
 |--------------------------------------------------------------------------
