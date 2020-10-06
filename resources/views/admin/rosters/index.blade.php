60ee7a6782bcabe17d8e322f731ba7f272f8652c
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2020-10-06 17:24:09 -0500

    .

--- resources/views/admin/rosters/index.blade.php
+++ resources/views/admin/rosters/index.blade.php
@@ -4,7 +4,7 @@
 @section("contenido") 
 
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">INDEX ADMIN ROSTER</h1>
+    <h1 class="h2">INDEX ADMIN ROSTERR</h1>
     <!--<p id="demo">a</p>-->
     <div class="btn-toolbar mb-2 mb-md-0">
       <div class="btn-group mr-2">        
