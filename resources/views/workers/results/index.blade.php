b862146aea39ab28c148b1e504782d011e6f6e19
yanet48 <59119371+yanet48@users.noreply.github.com>
2021-09-18 12:00:05 -0500

    Update index.blade.php

--- resources/views/workers/results/index.blade.php
+++ resources/views/workers/results/index.blade.php
@@ -11,10 +11,9 @@
         <div class="btn-group mr-2">        
           <a href="results/create" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Añadir Resultado</a>
         </div>   
-        {!! Form::submit('Consultar', ['class' => 'btn btn-primary' ]) !!}    
-             <a href="load/{{$trabajador->id}}" class="btn btn-success">Cargar Resultados</a>  
-        {!! Form::close() !!}
-  
+         <div class="btn-group mr-2">   
+          <button class="btn btn-success btn-submit">Cargar Resultado</button>
+        </div>
        </div> 
        
     @endif
