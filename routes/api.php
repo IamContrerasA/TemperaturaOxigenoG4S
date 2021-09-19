c795c55b606de9fafe5b90ca1ef4f2aa92753771
yanet48 <59119371+yanet48@users.noreply.github.com>
2021-09-19 15:16:26 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResultsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::apiResource("results","ResultsController")
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+ 
+ Route::post('results', [ResultsController::class, 'apiload']);
