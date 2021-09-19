228e3f037d1b19c7a78bfe0d4b0140bb957758e6
yanet48 <59119371+yanet48@users.noreply.github.com>
2021-09-19 14:20:10 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
- Route::post('results, 'ResultsController@apidata');
+ 
+Route::group(['middleware' => 'auth:api'], function() {    
+    Route::post('results', 'ResultsController@apidata');    
+});
+
