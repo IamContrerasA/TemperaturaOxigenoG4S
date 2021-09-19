210613467251939470516aae46772129a24f0c61
yanet48 <59119371+yanet48@users.noreply.github.com>
2021-09-19 14:05:54 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,4 @@ use App\Http\Controllers\ResultsController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
- Route::post('workers/results/load', [ResultsController::class, 'load'])->name('results.load');
- Route::post('load, 'ResultsController@apidata');
+ Route::post('results, 'ResultsController@apidata');
